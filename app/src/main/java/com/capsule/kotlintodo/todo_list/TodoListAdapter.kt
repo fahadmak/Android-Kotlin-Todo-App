c9c9e02b8345494ce809e6f34800261e8ddc7d5c
fahadmak <fahad.makabugo@andela.com>
2019-09-05 15:11:11 +0300

    chore(Data binding): bind ui layouts to data sources

--- app/src/main/java/com/capsule/kotlintodo/todo_list/TodoListAdapter.kt
+++ app/src/main/java/com/capsule/kotlintodo/todo_list/TodoListAdapter.kt
@@ -1,21 +1,19 @@
 package com.capsule.kotlintodo.todo_list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.capsule.kotlintodo.R
 import com.capsule.kotlintodo.data.models.Todo
-import kotlinx.android.synthetic.main.todo_item.view.*
+import com.capsule.kotlintodo.databinding.TodoItemBinding
 
 class TodoListAdapter(private val todoList: List<Todo>): RecyclerView.Adapter<TodoListAdapter.TodoHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoHolder {
         val inflater: LayoutInflater = LayoutInflater.from(parent.context)
 
-        val view: View = inflater.inflate(R.layout.todo_item, parent, false)
+        val binding: TodoItemBinding = TodoItemBinding.inflate(inflater, parent, false)
 
-        return TodoHolder(view)
+        return TodoHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -23,11 +21,17 @@ class TodoListAdapter(private val todoList: List<Todo>): RecyclerView.Adapter<To
     }
 
     override fun onBindViewHolder(holder: TodoHolder, position: Int) {
-        val todo: Todo = todoList.get(position)
-        holder.itemView.checkBox.text = todo.title
-        holder.itemView.checkBox.isChecked = todo.completed
+        val todo: Todo = todoList[position]
+        holder.bind(todo)
     }
 
-    class TodoHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    class TodoHolder(private val itemBinding: TodoItemBinding)
+        : RecyclerView.ViewHolder(itemBinding.root) {
+
+        fun bind(todo: Todo) {
+            itemBinding.todo = todo
+            itemBinding.executePendingBindings()
+        }
+    }
 }
 
